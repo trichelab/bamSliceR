300f88e0966d565f20687832fe809aae0de79451
huangyizR <peterhuang319@gmail.com>
2023-07-30 21:59:06 -0400

    add sample types constant

---
+++ R/constants.r
@@ -0,0 +1,12 @@
+primary = c("Primary Blood Derived Cancer - Bone Marrow", "Primary Blood Derived Cancer - Peripheral Blood")
+recurrent = c("Recurrent Blood Derived Cancer - Bone Marrow", "Recurrent Blood Derived Cancer - Peripheral Blood")
+cellline = c("Cell Lines")
+normal = c("Blood Derived Normal", "Bone Marrow Normal")
+model = c("Next Generation Cancer Model")
+posttreatment = c("Blood Derived Cancer - Peripheral Blood, Post-treatment", 
+                c("Blood Derived Cancer - Bone Marrow, Post-treatment")
+
+SAMPLE_TYPE = list( "primary" = primary, "recurrent" = recurrent, "cellline" = cellline,
+                    "normal" = normal, "model" = model, posttreatment = "posttreatment")
+
+
