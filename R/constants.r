258c2bfe312de7bf8a29b9a1078eb5d9e7d2c3b1
huangyizR <peterhuang319@gmail.com>
2023-07-30 22:07:34 -0400

    error in constant.r fixed

--- R/constants.r
+++ R/constants.r
@@ -4,9 +4,9 @@ cellline = c("Cell Lines")
 normal = c("Blood Derived Normal", "Bone Marrow Normal")
 model = c("Next Generation Cancer Model")
 posttreatment = c("Blood Derived Cancer - Peripheral Blood, Post-treatment", 
-                c("Blood Derived Cancer - Bone Marrow, Post-treatment")
+                "Blood Derived Cancer - Bone Marrow, Post-treatment")
 
-SAMPLE_TYPE = c( "primary" = primary, "recurrent" = recurrent, "cellline" = cellline,
-                    "normal" = normal, "model" = model, "posttreatment" = posttreatment)
+SAMPLE_TYPE = c( "primary" = primary, "recurrent" = recurrent, "cellline" = cellline, 
+                 "normal" = normal, "model" = model, "posttreatment" = posttreatment) 
 
 
