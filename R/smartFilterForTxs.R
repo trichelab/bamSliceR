53bd3e4765533496c6a17f1dc09a66e1257f5c70
huangyizR <peterhuang319@gmail.com>
2024-06-25 11:35:24 -0400

    add smartFilterForTxs.R

---
+++ R/smartFilterForTxs.R
@@ -0,0 +1,45 @@
+#' Filter variants based on VAF and altDepth cutoff and would keep all variants
+#' if at least one variant with annotated transcripts passed the cutoff. 
+#'
+#' @param res VRranges object from tallied reads of BAM files.
+#' @param VAF_cutoff numeric VAF cutoff.
+#' @param altDepth_cutoff numeric altDepth cutoff.
+#' @param gencode.file A gencode file in GFF3 format to be used for annotating variants. The
+#' input gff3 file for this function should contains coordinates information for both genomic and transcriptome,
+#' which can be done by bamSliceR::getTxsCoordsFromGFF(isSaveGenomicCoords = TRUE).
+#'
+#' @return VRanges object
+#'
+#' @export
+
+smartFilter = function(res, VAF_cutoff, altDepth_cutoff, gencode.file = "")
+{
+  keep = columns <- c(
+    "ref", 
+    "alt", 
+    "totalDepth", 
+    "refDepth", 
+    "altDepth", 
+    "VAF", 
+    "sample", 
+    "file_name", 
+    "case_id", 
+    "sample_type", 
+    "experimental_strategy", 
+    "downloaded_file_name", 
+    "UPC_ID"
+  )
+  res.addgenomic = getGenCodeAnnotation.Txs(res = res, gencode.file = gencode.file)
+  res.filtered.addgenomic = subset(res.addgenomic, VAF > VAF_cutoff) %>% subset(altDepth > altDepth_cutoff)
+  #res.filtered.addgenomic = getGenCodeAnnotation.Txs(res = res_filtered, gencode.file = gencode.file)
+  # TAG for unique locus on unique patients
+  res.filtered.addgenomic$tag = str_c(res.filtered.addgenomic$g_seqid, ":", 
+                                      res.filtered.addgenomic$g_start, ":",
+                                      res.filtered.addgenomic$g_end, ":", as.character(sampleNames(res.filtered.addgenomic) ))
+  res.addgenomic$tag = str_c(res.addgenomic$g_seqid, ":", 
+                             res.addgenomic$g_start, ":",
+                             res.addgenomic$g_end, ":", as.character(sampleNames(res.addgenomic) ))
+  res.addgenomic = subset(res.addgenomic, tag %in% res.filtered.addgenomic$tag)[,keep]
+  res.addgenomic
+}
+
