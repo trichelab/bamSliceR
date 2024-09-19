7602d2840a0046e99a8c968a20c61656d2879c46
huangyizR <peterhuang319@gmail.com>
2024-09-19 12:42:17 -0400

    add option in fixIndelRefCounts that also pileup corresponding variants counts.

--- R/fixIndelRefCounts.R
+++ R/fixIndelRefCounts.R
@@ -6,6 +6,7 @@
 #' @param dir string directory of the BAM files.
 #' @param mode either "ALL" for all types of variants, or "INDEL", which only perform pileup on INDEL 
 #' variants to speed up the process.
+#' @param addAltDepth bool TRUE if want to also tally alt counts
 #' @param isFlank ranges to pileup and calculate means of total read Depth for the variants.
 #' @param totalDepthOnly if TRUE, then would not calculate VAF.
 #' @param PileupParam same as PileupParam in Rsamtools.
@@ -15,7 +16,7 @@
 #'
 #' @export
 
-fixIndelRefCounts = function(gr,dir = "./", mode = c("ALL", "INDEL"), 
+fixIndelRefCounts = function(gr,dir = "./", mode = c("ALL", "INDEL"), addAltDepth = FALSE, 
                              isFlank = FALSE, totalDepthOnly = TRUE, PileupParam = NA, mc.cores = 1)
 {
   if (is.na(PileupParam  ) )
@@ -35,8 +36,8 @@ fixIndelRefCounts = function(gr,dir = "./", mode = c("ALL", "INDEL"),
     # make sure no overlapped ranges, otherwise pileup would double counts #
     which_ranges = disjoin(x)
     gp <- ScanBamParam(which=which_ranges, what=scanBamWhat(), flag = scanBamFlag(isDuplicate = FALSE) )
-    pup =  pileup(file, scanBamParam=gp, pileupParam=p )
-    pup = aggregate(count ~ seqnames + pos, data = pup, FUN = sum)
+    pup_raw =  pileup(file, scanBamParam=gp, pileupParam=p )
+    pup = aggregate(count ~ seqnames + pos, data = pup_raw, FUN = sum)
     pup$start = pup$pos
     pup$end = pup$pos
     pup$pos = NULL
@@ -53,6 +54,56 @@ fixIndelRefCounts = function(gr,dir = "./", mode = c("ALL", "INDEL"),
       ori_x[ori_x_vaf1_IDX]$totalDepth = ori_x[ori_x_vaf1_IDX]$altDepth
       ori_x$refDepth = ori_x$totalDepth - ori_x$altDepth
       ori_x$VAF = ori_x$altDepth/ori_x$totalDepth
+    }
+    if (addAltDepth) 
+    {
+      if (is.null(ori_x$altDepth ))
+      {
+        ori_x$altDepth = 0
+      }
+      getVarType(ori_x) -> varType
+      SNP_IDX = which(varType == "SNP")
+      INS_IDX = which(varType == "INS")
+      DEL_IDX = which(varType == "DEL")
+      if (length(DEL_IDX) != 0 )
+      {
+        pup_del = subset(pup_raw, nucleotide == "-")
+        pup_true_del_IDX = which(GRanges(pup_del$which_label) %>% ranges() %>% width() != 1)
+        pup_true_del = pup_del[pup_true_del_IDX,] 
+        pup_true_del = pup_true_del[-which(duplicated(pup_true_del$which_label)), ]
+        pup_true_del_gr = GRanges(pup_true_del$which_label)
+        pup_true_del_gr$altDepth = pup_true_del$count
+        hits = findOverlaps(ori_x, pup_true_del_gr, type = "equal")
+        ori_x[queryHits(hits), ]$altDepth = pup_true_del_gr[subjectHits(hits)]$altDepth
+      }
+      if (length(INS_IDX) != 0)
+      {
+        pup_ins = subset(pup_raw, nucleotide == "+")
+        pup_ins_gr = GRanges(pup_ins$which_label)
+        pup_ins_gr$altDepth = pup_ins$count
+        hits_df = as.data.frame(findOverlaps(ori_x, pup_ins_gr, type = "equal"))
+        ins_hit_IDX = which(hits_df$queryHits %in% INS_IDX)
+        if (length(ins_hit_IDX) > 0)
+        {
+          hits_df = hits_df[ins_hit_IDX,] 
+          ori_x[hits_df$queryHits, ]$altDepth = pup_ins_gr[hits_df$subjectHits,]$altDepth 
+        }
+      }
+      if (length(SNP_IDX) != 0)
+      {
+        pup_raw_snp = subset(pup_raw, !(pup_raw$nucleotide %in% c("-", "+")  ) )
+        alt_base_coords_tag = str_c(as.character(seqnames(ori_x)), ":", start(ori_x), ":", ori_x$alt)
+        pup_raw_snp$alt_base_coords_tag = str_c(pup_raw_snp$seqnames,":", pup_raw_snp$pos , ":", pup_raw_snp$nucleotide)
+        rownames(pup_raw_snp) = pup_raw_snp$alt_base_coords_tag
+        pup_raw_snp[alt_base_coords_tag[SNP_IDX],] -> pup_alt_counts
+        which( is.na(pup_alt_counts$count) ) -> missed_count_IDX
+        if ( length(missed_count_IDX) > 0)
+        {
+          pup_alt_counts[missed_count_IDX,]$count = 0 
+        }
+        pup_alt_counts$ori_tag = alt_base_coords_tag[SNP_IDX]
+        ori_x[SNP_IDX]$altDepth = pup_alt_counts$count
+      }
     }
     ori_x
   }
