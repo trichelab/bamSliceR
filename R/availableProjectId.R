cc9b4e3333d0804ff0bf879051442c793022eeac
huangyizR <peterhuang319@gmail.com>
2024-09-20 17:47:47 -0400

    add example.

--- R/availableProjectId.R
+++ R/availableProjectId.R
@@ -1,5 +1,8 @@
 #' Check the available projects on GDC portal
 #' @return a vector of available projects on GDC portal
+#' @examples
+#' ava_pID = availableProjectId()
+#' head(ava_pID)
 #' @export
 
 availableProjectId = function()
